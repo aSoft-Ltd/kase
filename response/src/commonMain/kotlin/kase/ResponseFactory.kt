2ec0e3dde9253c8b453cca319cb106addde4b412
andylamax <andylamax@programmer.net>
2023-10-08 17:32:57 +0300

    Taking a pause for a while

--- response/src/commonMain/kotlin/kase/ResponseFactory.kt
+++ response/src/commonMain/kotlin/kase/ResponseFactory.kt
@@ -2,4 +2,22 @@
 
 package kase
 
+import kollections.iEmptyList
+import kollections.iListOf
+
 inline fun <D> responseOf(value: D): Response<D> = Successful(value)
+
+inline fun Throwable.toFailed(code: Int = 400, message: String = this.message ?: "Error") = Failed(
+    status = Status(code, message),
+    errors = cause.toErrors()
+)
+
+private fun Throwable.toError() = Error(
+    message = message ?: "Unknown",
+    type = this::class.simpleName ?: "Unknown",
+    cause = cause?.message ?: "Unknown",
+    stackTrace = stackTraceToString()
+)
+
+@PublishedApi
+internal fun Throwable?.toErrors() = if (this != null) iListOf(toError()) else iEmptyList()

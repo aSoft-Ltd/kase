29055d1b3638387eaf93c93375cdf309db265864
andylamax <andylamax@programmer.net>
2023-10-10 17:04:47 +0300

    Fixed failing tests for RegistrationServiceFlix

--- response/core/src/commonMain/kotlin/kase/Status.kt
+++ response/core/src/commonMain/kotlin/kase/Status.kt
@@ -12,6 +12,7 @@ data class Status(
 ) {
     companion object {
         val CODE_200_Ok by lazy { Status(200, "Ok") }
+        val CODE_401_Unauthorized by lazy { Status(401,"Unauthorized") }
         val CODE_409_Conflict by lazy { Status(409, "Conflict") }
     }
 }

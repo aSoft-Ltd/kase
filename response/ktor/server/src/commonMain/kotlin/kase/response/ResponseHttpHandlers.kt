eae0bce12dfcbdeeb9d6379d8ccd7f1d059000c1
andylamax <andylamax@programmer.net>
2024-03-28 15:30:33 +0300

    Resolving sme services all together

--- response/ktor/server/src/commonMain/kotlin/kase/response/ResponseHttpHandlers.kt
+++ response/ktor/server/src/commonMain/kotlin/kase/response/ResponseHttpHandlers.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.ApplicationCall
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
+import io.ktor.server.routing.patch
 import io.ktor.util.pipeline.PipelineContext
 import kotlinx.serialization.StringFormat
 
@@ -12,6 +13,12 @@ inline fun <reified D> Routing.post(
     codec: StringFormat,
     noinline block: suspend PipelineContext<*, ApplicationCall>.() -> D
 ) = post(path) { respondJson(codec) { block() } }
+
+inline fun <reified D> Routing.patch(
+    path: String,
+    codec: StringFormat,
+    noinline block: suspend PipelineContext<*, ApplicationCall>.() -> D
+) = patch(path) { respondJson(codec) { block() } }
 
 inline fun <reified D> Routing.get(
     path: String,

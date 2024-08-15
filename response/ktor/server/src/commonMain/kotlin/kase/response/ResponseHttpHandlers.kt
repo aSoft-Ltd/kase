ab98130f34dbb5b56c930a2d1a4c6a5343616d26
george <george.sechu@gmail.com>
2024-08-15 12:19:38 +0300

    several changes

--- response/ktor/server/src/commonMain/kotlin/kase/response/ResponseHttpHandlers.kt
+++ response/ktor/server/src/commonMain/kotlin/kase/response/ResponseHttpHandlers.kt
@@ -9,23 +9,23 @@ import kotlinx.serialization.StringFormat
 inline fun <reified D> Routing.post(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend PipelineContext<*, ApplicationCall>.() -> D
 ) = post(path) { respondJson(codec) { block() } }
 
 inline fun <reified D> Routing.patch(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend PipelineContext<*, ApplicationCall>.() -> D
 ) = patch(path) { respondJson(codec) { block() } }
 
 inline fun <reified D> Routing.delete(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend PipelineContext<*, ApplicationCall>.() -> D
 ) = delete(path) { respondJson(codec) { block() } }
 
 inline fun <reified D> Routing.get(
     path: String,
     codec: StringFormat,
-    noinline block: suspend RoutingContext.() -> D
+    noinline block: suspend PipelineContext<*, ApplicationCall>.() -> D
 ) = get(path) { respondJson(codec) { block() } }
